5aa4588d4d3d10b2b635d8663e7856df7a030824
DenoxM <aurelien34290@gmail.com>
2023-02-16 11:03:15 +0100

    FEAT Commande:Ajout CommandeEffacer

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -37,4 +37,10 @@ public class Document {
         String remplacement = this.texte.substring(start,end);
         this.texte = leftPart + remplacement.toUpperCase() + rightPart;
     }
+
+    public void effacer(int start, int end) {
+        String leftPart = this.texte.substring(0, start);
+        String rightPart = this.texte.substring(end);
+        this.texte = leftPart + rightPart;
+    }
 }
