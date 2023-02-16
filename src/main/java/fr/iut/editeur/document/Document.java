1164f260acc399ccf6af3ea8e99b0b9fd9175901
DenoxM <aurelien34290@gmail.com>
2023-02-16 12:27:02 +0100

    DOCS Commandes: Mise en place documentation

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -15,7 +15,7 @@ public class Document {
     public void setTexte(String texte) {
         this.texte = texte;
     }
-
+    /* Methode qui permet d'ajouter du texte */
     public void ajouter(String texte) {
         this.texte += texte;
     }
@@ -24,13 +24,13 @@ public class Document {
     public String toString() {
         return this.texte;
     }
-
+    /* Methode qui permet de remplacer du texte entre deux points donnés */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = this.texte.substring(0, start);
         String rightPart = this.texte.substring(end);
         this.texte = leftPart + remplacement + rightPart;
     }
-
+    /* Methode qui permet de mettre du texte en majuscule entre deux points donnés */
     public void majuscules(int start, int end) {
         String leftPart = this.texte.substring(0, start);
         String rightPart = this.texte.substring(end);
@@ -38,12 +38,14 @@ public class Document {
         this.texte = leftPart + remplacement.toUpperCase() + rightPart;
     }
 
-
+    /* Methode qui permet d'éffacer entre deux points donnés */
     public void effacer(int start, int end) {
         String leftPart = this.texte.substring(0, start);
         String rightPart = this.texte.substring(end);
         this.texte = leftPart + rightPart;
     }
+
+    /* Methode qui permet de tout supprimer */
     public void clear() {
         this.texte = "";
     }
